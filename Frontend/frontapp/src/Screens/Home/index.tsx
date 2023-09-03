faed9edbd7975826aa699c32ba03bdb2ee2684d6
Fede Arron <federicoarron15@gmail.com>
2023-09-03 17:08:02 -0300

    New ignore files and login screen

--- Frontend/frontapp/src/Screens/Home/index.tsx
+++ Frontend/frontapp/src/Screens/Home/index.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="text-center">
+    <div className="container mx-auto object-center">
         <h1 className="text-2xl font-bold underline">
           Home
         </h1>
--- Frontend/frontapp/src/Screens/Login/index.tsx
+++ Frontend/frontapp/src/Screens/Login/index.tsx
@@ -1,9 +1,19 @@
+import { Button, Card, CardBody, CardHeader, Divider, Input } from "@nextui-org/react"
+
 function Login() {
   return (
-    <div>
-        <h1 className="text-2xl font-bold underline">
-            Login
-        </h1>
+    <div className="flex items-center justify-center h-screen">
+        <Card className="w-full max-w-md">
+          <CardHeader className="flex items-center justify-center">
+            <h1 className="text-2xl font-bold">Log In</h1>
+          </CardHeader>
+          <Divider />
+          <CardBody className="w-full">
+            <Input type="email" label="Email" className="p-2 text-2xl" />
+            <Input type="password" label="Password" className="p-2 text-2xl" />
+            <Button>Sign in</Button>
+          </CardBody>
+        </Card>
     </div>
   )
 }
