2e43d9f3ad16316ad09bfb05077b6636f067d67c
Fede Arron <federicoarron15@gmail.com>
2023-12-12 17:57:04 -0300

    Fixed information modal title

--- Frontend/frontapp/src/Components/InformationModal/index.jsx
+++ Frontend/frontapp/src/Components/InformationModal/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Modal, ModalContent, ModalBody, ModalHeader, useDisclosure } from "@nextui-org/react"
 
-const InformationModal = ({open, setOpen, children}) => {
+const InformationModal = ({open, setOpen, children, title }) => {
     const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
 
     useEffect(() => {
@@ -24,7 +24,7 @@ const InformationModal = ({open, setOpen, children}) => {
           <ModalContent>
             {() => (
               <>
-                <ModalHeader className="flex gap-1 max-h-[700px]">Health Assesment</ModalHeader>
+                <ModalHeader className="flex gap-1 max-h-[700px]">{title}</ModalHeader>
                 <ModalBody className="flex flex-row max-h-[700px] justify-center">
                   { children }
                 </ModalBody>
